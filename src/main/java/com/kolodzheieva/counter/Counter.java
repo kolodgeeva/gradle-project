b342c7b3daccf1c361b6c3c13cf33a7f14e68153
mariana <kolodgeeva@gmail.com>
2020-01-25 14:48:24 +0200

    Add junit

---
+++ src/main/java/com/kolodzheieva/counter/Counter.java
@@ -0,0 +1,15 @@
+package com.kolodzheieva.counter;
+
+public class Counter {
+
+    public static int COUNT;
+
+    public static void increment() {
+        COUNT++;
+    }
+
+    public static void decrement() {
+        COUNT--;
+    }
+
+}
---
+++ src/test/java/com/kolodzheieva/counter/CounterTest.java
@@ -0,0 +1,11 @@
+package com.kolodzheieva.counter;
+
+import org.junit.jupiter.api.Test;
+
+class CounterTest {
+
+    @Test
+    void testIncrement() {
+
+    }
+}
